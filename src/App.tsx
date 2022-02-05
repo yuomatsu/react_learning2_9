de1ffa763b135711c1dfb200d5bbf2369f9fe042
yuomatsu <96334534+yuomatsu@users.noreply.github.com>
2022-02-05 21:16:10 +0900

    47. 返却値

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./styles.css";
+import { Practice1 } from "./practices/Practice1";
+import { Practice2 } from "./practices/Practice2";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Practice1 />
+      <Practice2 />
     </div>
   );
 }
